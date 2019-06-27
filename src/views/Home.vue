da159d1910231897fae5610d1ca55cc70fcbb4d3
xyb001 <1730829742@qq.com>
2019-06-27 09:28:58 +0800

    'test'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world>
+      <template v-slot="{qqq: a}">
+        {{a}}
+      </template>
+    </hello-world>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  created () {
+  },
   components: {
     HelloWorld
   }
